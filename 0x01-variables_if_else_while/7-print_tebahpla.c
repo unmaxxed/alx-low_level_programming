fd7f933a47cf8087aa19bda6edf6eb496c5c8084
Emmanuel O <unmaxxed2@gmail.com>
2023-06-19 03:06:26 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
