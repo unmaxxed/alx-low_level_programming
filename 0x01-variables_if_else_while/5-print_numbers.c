c67217eb0b5b937b52ca796dd1f5565ca0d4cf4d
Emmanuel O <unmaxxed2@gmail.com>
2023-06-19 02:43:49 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit numbers starting from 0
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	printf("%d", i);
+
+	return (0);
+}
+
