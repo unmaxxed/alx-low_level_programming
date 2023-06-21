d41bfc3c44d90b73650ee41b94fa988bd7956c4d
Emmanuel O <unmaxxed2@gmail.com>
2023-06-21 12:36:31 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all letters in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar(letter);
+
+	_putchar('\n');
+
+	return (0);
+}
